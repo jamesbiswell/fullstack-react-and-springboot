202a043b3989d4baeb36af6076da76d0865b1af7
jamesbiswell <43172281+jamesbiswell@users.noreply.github.com>
2026-06-12 12:43:05 +0100

    Adding section-19-library-project-home-page-refactor

--- source-code/full-stack-library-app/03-frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
+++ source-code/full-stack-library-app/03-frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
@@ -1,7 +1,9 @@
 import {useAuth0} from "@auth0/auth0-react";
+import {Link} from "react-router-dom";
 
 export const LibraryServices = () => {
 
+    const { isAuthenticated } = useAuth0();
     const { getIdTokenClaims } = useAuth0();
 
     return (
@@ -16,9 +18,15 @@ export const LibraryServices = () => {
                         send our library admin's a personal message!
                     </p>
                     <div className='d-grid gap-2 justify-content-md-start mb-4 mb-lg-3'>
-                        <a className='btn main-color btn-lg text-white' href='#'>
-                            Sign up
-                        </a>
+                        {isAuthenticated ?
+                            <Link to='#' type='button' className='btn main-color btn-lg px-4 me-md-2 fw-bold text-white'>
+                                Library Services
+                            </Link>
+                            :
+                            <Link className='btn main-color btn-lg text-white' to='/login'>
+                                Sign up
+                            </Link>
+                        }
                     </div>
                 </div>
                 <div className='col-lg-4 offset-lg-1 shadow-lg lost-image'></div>
