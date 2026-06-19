b84647a220fc51963079b21c2381680dbd6e7729
jamesbiswell <43172281+jamesbiswell@users.noreply.github.com>
2026-06-19 12:45:58 +0100

    Continuing section-35-library-project-stripe-payment-processing

--- source-code/full-stack-library-app/02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/AdminController.java
+++ source-code/full-stack-library-app/02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/AdminController.java
@@ -24,48 +24,37 @@ public class AdminController {
     @PutMapping("/secure/increase/book/quantity")
     public void increaseBookQuantity(@AuthenticationPrincipal Jwt jwt,
                                      @RequestParam Long bookId) throws Exception {
+        jwtGetClaimAsList(jwt);
+        adminService.increaseBookQuantity(bookId);
+    }
+    
+    private void jwtGetClaimAsList(@AuthenticationPrincipal Jwt jwt) throws Exception {
         List<String> roles = jwt.getClaimAsStringList("https://james-react-library.com/roles");
         String admin = roles != null && !roles.isEmpty() ? roles.get(0) : null;
         
         if (admin == null || !admin.equals("admin")) {
             throw new Exception("Administration page only");
         }
-        adminService.increaseBookQuantity(bookId);
     }
     
     @PutMapping("/secure/decrease/book/quantity")
     public void decreaseBookQuantity(@AuthenticationPrincipal Jwt jwt,
                                      @RequestParam Long bookId) throws Exception {
-        List<String> roles = jwt.getClaimAsStringList("https://james-react-library.com/roles");
-        String admin = roles != null && !roles.isEmpty() ? roles.get(0) : null;
-        
-        if (admin == null || !admin.equals("admin")) {
-            throw new Exception("Administration page only");
-        }
+        jwtGetClaimAsList(jwt);
         adminService.decreaseBookQuantity(bookId);
     }
     
     @PostMapping("/secure/add/book")
     public void postBook(@AuthenticationPrincipal Jwt jwt,
                          @RequestBody AddBookRequest addBookRequest) throws Exception {
-        List<String> roles = jwt.getClaimAsStringList("https://james-react-library.com/roles");
-        String admin = roles != null && !roles.isEmpty() ? roles.get(0) : null;
-
-        if (admin == null || !admin.equals("admin")) {
-            throw new Exception("Administration page only");
-        }
+        jwtGetClaimAsList(jwt);
         adminService.postBook(addBookRequest);
     }
     
     @DeleteMapping("/secure/delete/book")
     public void deleteBook(@AuthenticationPrincipal Jwt jwt,
                            @RequestParam Long bookId) throws Exception {
-        List<String> roles = jwt.getClaimAsStringList("https://james-react-library.com/roles");
-        String admin = roles != null && !roles.isEmpty() ? roles.get(0) : null;
-        
-        if (admin == null || !admin.equals("admin")) {
-            throw new Exception("Administration page only");
-        }
+        jwtGetClaimAsList(jwt);
         adminService.deleteBook(bookId);
     }
 }
