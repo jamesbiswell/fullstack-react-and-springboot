1a3eccf1373b1d6186987674abc06034ce0ac301
jamesbiswell <43172281+jamesbiswell@users.noreply.github.com>
2026-06-19 15:56:33 +0100

    Continuing section-35-library-project-stripe-payment-processing

--- source-code/full-stack-library-app/03-frontend/react-library/src/index.tsx
+++ source-code/full-stack-library-app/03-frontend/react-library/src/index.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY!);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
     <BrowserRouter>
-        <App />
+        <Elements stripe={stripePromise}>
+            <App/>
+        </Elements>
     </BrowserRouter>
 );
